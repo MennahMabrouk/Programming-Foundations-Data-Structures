d5f9f64d309ae5878a872dca960c12e446c91016
MennahTullah Sameh <101124995+MennahTulla@users.noreply.github.com>
2022-04-30 10:44:49 +0200

    Update Multidimensional arrays in C++.cpp

--- Multidimensional arrays in C++.cpp
+++ Multidimensional arrays in C++.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stdio.h>
-#include<string>
+#include <string>
 using namespace std;
 
 int main()
 {
 	
 	string dinnerChoices[2][3] = { {"Salad", "Soup", "Cheese Plate"},
-				       {"Chicken", "Salmon", "Lasagna"}};
+				                   {"Chicken", "Salmon", "Lasagna"} };
 	cout << dinnerChoices[0][0]<<"\n"<< dinnerChoices[0][1] << "\n"<<dinnerChoices[1][2]<<"\n" << "\n";
 
 	for (int i = 0; i < 4; i++)
